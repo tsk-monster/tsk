4e1b73a32bb0f534deb6ee446c36b616cae1114a
Gilad Kutiel <giladk@mobileye.com>
2024-04-09 12:21:45 +0300

    running tsks

--- tsk/tsk.py
+++ tsk/tsk.py
@@ -1,22 +1,53 @@
-from dataclasses import dataclass
-from typing import Any, Generator, Set
+from collections import defaultdict, deque
+from typing import Any, Generator, Tuple
 
-tsk = Generator[Any, None, None]
-
-
-@dataclass
-class pull:
-    pass
+tsk = Generator[Tuple[bool, Any], None, None]
 
 
-@dataclass
-class push:
-    pass
+def run(tsks: deque[tsk]):
+    wait = defaultdict(list)
+    done = set()
+
+    while tsks:
+        try:
+            tsk = tsks.popleft()
+            need, val = next(tsk)
+            if need and val not in done:
+                wait[val].append(tsk)
+            else:
+                done.add(val)
+                for w in wait.pop(val, []):
+                    tsks.append(w)
+                tsks.append(tsk)
+        except StopIteration:
+            pass
+
+    if wait:
+        print('Could not finish all tasks.')
+    else:
+        print('All tasks finished.')
 
 
-def run(tsks: Set[tsk]):
-    print("Hello World!")
+def pull(val):
+    return (True, val)
+
+
+def push(val):
+    return (False, val)
 
 
 if __name__ == '__main__':
-    run(set())
+    print()
+    print('start')
+
+    def tsk1():
+        yield push('tsk1')
+        yield pull('tsk1')
+        print('tsk1')
+
+    def tsk2():
+        yield push('tsk2')
+        yield pull('tsk2')
+        print('tsk2')
+
+    run(deque([tsk1(), tsk2()]))
