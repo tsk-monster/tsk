e8a25f23b7ec334b99178e79e454066b4da0305f
Gilad Kutiel <giladk@mobileye.com>
2024-04-09 14:09:59 +0300

    change

--- tsk/tsks.py
+++ tsk/tsks.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 
 def pull(val):
@@ -9,6 +10,17 @@ def push(val):
     return (False, val)
 
 
+def change(path: Path):
+    try:
+        tsk = path.with_suffix('.tsk')
+        if not tsk.exists():
+            return True
+
+        return tsk.stat().st_mtime < path.stat().st_mtime
+    finally:
+        tsk.touch()
+
+
 def shell(cmd, *, deps=[], prods=[]):
     for d in deps:
         yield pull(d)
@@ -17,3 +29,8 @@ def shell(cmd, *, deps=[], prods=[]):
 
     for p in prods:
         yield push(p)
+
+
+if __name__ == '__main__':
+    print(change(Path('tmp.txt')))
+    print(change(Path('tmp.txt')))
