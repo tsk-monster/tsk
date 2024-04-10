4d6929a57eb3a7e6539d927ef0830cead0725309
Gilad Kutiel <gilad.kutiel@gmail.com>
2024-04-10 23:11:59 +0300

    logging

--- example.py
+++ example.py
@@ -1,17 +1,15 @@
-import asyncio
 import logging
+from pathlib import Path
 
-from tsk_monster import plan, shell
+from tsk_monster import run, shell
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        level=logging.INFO,
+        datefmt='%H:%M:%S',
+        format='%(asctime)s [%(levelname)s] %(message)s')
 
-    print()
-    print('-----')
-    asyncio.run(
-        plan([
-            shell('wget https://picsum.photos/200'),
-            shell('wget https://picsum.photos/200'),
-            shell('wget https://picsum.photos/200'),
-            shell('wget https://picsum.photos/200'),
-            shell('wget https://picsum.photos/200')], parallelism=10))
+    run(
+        shell(
+            'wget -o img1.jpg https://picsum.photos/200',
+            make=[Path('img1.jpg')]))
--- tsk_monster/__init__.py
+++ tsk_monster/__init__.py
@@ -3,24 +3,24 @@ import logging
 from collections import defaultdict, deque
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, AsyncGenerator, Awaitable, Callable, Iterable, List
+from typing import (Any, AsyncGenerator, Awaitable, Callable, Iterable, List,
+                    Union)
 
-lg = logging.getLogger('👾')
+lg = logging.getLogger(__name__)
 
 
 @dataclass
-class Need:
+class input:
     val: Any
 
 
 @dataclass
-class Make:
+class output:
     val: Any
 
 
-def changed(path: str | Path):
+def changed(path: Path):
     try:
-        path = Path(path)
         tsk = path.with_suffix('.tsk')
         if not tsk.exists():
             return True
@@ -37,7 +37,7 @@ def none(items: Iterable[bool]):
 @dataclass
 class Tsk:
     name: str
-    gen: AsyncGenerator
+    gen: AsyncGenerator[Union[input, output, "Tsk"], None]
 
     def __repr__(self):
         return self.name
@@ -45,23 +45,28 @@ class Tsk:
 
 def tsk(
         name: str, action: Callable[[], Awaitable], *,
-        need: List[Path] = [],
-        make: List[Path] = []):
+        inputs: List[Path] = [],
+        outputs: List[Path] = []):
 
     async def gen():
-        always_run = len(need) == len(make) == 0
+        always_run = len(inputs) == len(outputs) == 0
 
-        for n in need:
-            yield Need(n)
+        for n in inputs:
+            yield input(n)
 
-        if any(map(changed, need)) or not all(map(Path.exists, make)) or always_run:
+        if any(map(changed, inputs)) or not all(map(Path.exists, outputs)) or always_run:
+            lg.info(f'STARTING: {name}')
             await action()
+            lg.info(f'DONE: {name}')
 
-        for m in make:
+        else:
+            lg.info(f'SKIPPING: {name}')
+
+        for m in outputs:
             if m.exists():
-                yield Make(m)
+                yield output(m)
             else:
-                lg.warning(f'Failed to make {m}.')
+                lg.warning(f'Failed to create {m}.')
 
     return Tsk(name, gen())
 
@@ -73,34 +78,32 @@ def shell(
 
     return tsk(
         cmd, lambda: asyncio.create_subprocess_shell(cmd),
-        need=need,
-        make=make)
+        inputs=need,
+        outputs=make)
 
 
-async def plan(tsks: Iterable[Tsk], parallelism=1):
-
-    def step(tsk: Tsk):
-        lg.info(f'START: {tsk.name}')
-        return anext(tsk.gen)
+async def runner(tsks: Iterable[Tsk], parallelism=1):
 
     tsks = deque(tsks)
     wait = defaultdict(list)
     done = set()
 
     while tsks:
-        head = [tsks.popleft() for _ in range(min(parallelism, len(tsks)))]
+        batch = [
+            tsks.popleft()
+            for _ in range(min(parallelism, len(tsks)))]
 
         results = await asyncio.gather(
-            *(step(tsk) for tsk in head),
+            *(anext(tsk.gen) for tsk in batch),
             return_exceptions=True)
 
-        assert len(head) == len(results)
+        assert len(batch) == len(results)
 
-        for tsk, res in zip(head, results):
-            if isinstance(res, Need) and res.val not in done:
+        for tsk, res in zip(batch, results):
+            if isinstance(res, input) and res.val not in done:
                 wait[res.val].append(tsk)
 
-            elif isinstance(res, Make):
+            elif isinstance(res, output):
                 done.add(res.val)
                 for w in wait.pop(res.val, []):
                     tsks.append(w)
@@ -111,15 +114,17 @@ async def plan(tsks: Iterable[Tsk], parallelism=1):
                 tsks.append(tsk)
 
             elif isinstance(res, StopAsyncIteration):
-                lg.info(f'END: {tsk.name}')
+                pass
 
             else:
                 lg.error(f'Failed to run {tsk.name}: {res}')
 
     if wait:
-        print(f'Could not run all tasks: {wait}')
+        lg.warning(f'Could not run all tasks: {wait.values()}')
+        return
 
-    else:
-        print('All tasks finished.')
+    lg.info('All tasks finished.')
 
-    return wait
+
+def run(*tsks: Tsk, parallelism=1):
+    asyncio.run(runner(tsks, parallelism=parallelism))
