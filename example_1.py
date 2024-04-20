2a6316a856975e8eb6638aefa70d7708fc1b9727
Gilad Kutiel <gilad.kutiel@gmail.com>
2024-04-20 12:42:12 +0300

    Deployed 31fd6cf with MkDocs version: 1.5.3

--- example_1.py
+++ example_1.py
@@ -1,7 +1,7 @@
 from tsk_monster import tsk
 
 
-def download_images():
+def download_image():
     yield tsk(
         'wget -O large.jpg https://picsum.photos/200/300',
         prods=['large.jpg'])
